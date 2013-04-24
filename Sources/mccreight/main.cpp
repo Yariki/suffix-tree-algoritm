9936bee20ab2e543b0df80079740b18a1b95c62a
yariki <iyariki.ya@gmail.com>
2013-04-25 00:58:27 +0300

    added missed files

---
+++ Sources/mccreight/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <iostream>
+#include <time.h>
+
+using std::string;
+
+
+
+
+int main()
+{
+
+
+	system("pause");
+}
